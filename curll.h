8360badb6b48fde4e2fb435a22ff82ceaef9447c
Madman10K <stanv.contact176@gmail.com>
2024-09-15 04:00:03 +0300

    Move away from legacy naming

--- curll.h
+++ curll.h
@@ -1,17 +1,17 @@
 #pragma once
 
-#ifdef UVK_LOG_EXPORT_FROM_LIBRARY
+#ifdef MLS_EXPORT_LIBRARY
     #ifdef _WIN32
-        #ifdef UVK_LIB_COMPILE
-            #define UVK_PUBLIC_API __declspec(dllexport)
+        #ifdef MLS_LIB_COMPILE
+            #define MLS_PUBLIC_API __declspec(dllexport)
         #else
-            #define UVK_PUBLIC_API __declspec(dllimport)
+            #define MLS_PUBLIC_API __declspec(dllimport)
         #endif
     #else
-        #define UVK_PUBLIC_API
+        #define MLS_PUBLIC_API
     #endif
 #else
-    #define UVK_PUBLIC_API
+    #define MLS_PUBLIC_API
 #endif
 
 #ifdef __cplusplus
@@ -19,20 +19,20 @@ extern "C"
 {
 #endif
     // UntitledImGuiFramework Event Safety - Any time
-    UVK_PUBLIC_API void* urll_dlopen(const char* location);
+    MLS_PUBLIC_API void* urll_dlopen(const char* location);
 
     // UntitledImGuiFramework Event Safety - Any time
-    UVK_PUBLIC_API void* urll_dlsym(void* handle, const char* name);
+    MLS_PUBLIC_API void* urll_dlsym(void* handle, const char* name);
     // UntitledImGuiFramework Event Safety - Any time
-    UVK_PUBLIC_API void* urll_dlsym_func(void* handle, const char* name, void** function);
+    MLS_PUBLIC_API void* urll_dlsym_func(void* handle, const char* name, void** function);
     // UntitledImGuiFramework Event Safety - Any time
-    UVK_PUBLIC_API void* urll_dlsym_var(void* handle, const char* name, void* var);
+    MLS_PUBLIC_API void* urll_dlsym_var(void* handle, const char* name, void* var);
 
     // UntitledImGuiFramework Event Safety - Any time
-    UVK_PUBLIC_API int urll_dlclose(void* handle);
+    MLS_PUBLIC_API int urll_dlclose(void* handle);
 
     // UntitledImGuiFramework Event Safety - Any time
-    UVK_PUBLIC_API char* urll_dlerror();
+    MLS_PUBLIC_API char* urll_dlerror();
 #ifdef __cplusplus
 };
 #endif
--- urll.h
+++ urll.h
@@ -10,36 +10,32 @@
     #include <dlfcn.h>
 #endif
 
-#ifdef UVK_LOG_EXPORT_FROM_LIBRARY
+#ifdef MLS_EXPORT_LIBRARY
     #ifdef _WIN32
-        #ifdef UVK_LIB_COMPILE
-            #define UVK_PUBLIC_API __declspec(dllexport)
-            #define UVK_PUBLIC_TMPL_API __declspec(dllexport)
+        #ifdef MLS_LIB_COMPILE
+            #define MLS_PUBLIC_API __declspec(dllexport)
         #else
-            #define UVK_PUBLIC_API __declspec(dllimport)
-            #define UVK_PUBLIC_TMPL_API
+            #define MLS_PUBLIC_API __declspec(dllimport)
         #endif
     #else
-        #define UVK_PUBLIC_API
-        #define UVK_PUBLIC_TMPL_API
+        #define MLS_PUBLIC_API
     #endif
 #else
-    #define UVK_PUBLIC_API
-    #define UVK_PUBLIC_TMPL_API
+    #define MLS_PUBLIC_API
 #endif
 
 namespace URLL
 {
     // UntitledImGuiFramework Event Safety - Any time
-    UVK_PUBLIC_API void* dlopen(const char* location) noexcept;
+    MLS_PUBLIC_API void* dlopen(const char* location) noexcept;
 
     // UntitledImGuiFramework Event Safety - Any time
-    UVK_PUBLIC_API void* dlsym(void* handle, const char* name) noexcept;
+    MLS_PUBLIC_API void* dlsym(void* handle, const char* name) noexcept;
 
 #ifdef URLL_USE_FUNCTIONAL
     // UntitledImGuiFramework Event Safety - Any time
     template<typename T, typename... T2>
-    UVK_PUBLIC_TMPL_API void* dlsym(void* handle, const char* name, std::function<T(T2...)>& function) noexcept
+    void* dlsym(void* handle, const char* name, std::function<T(T2...)>& function) noexcept
     {
         void* ptr = dlsym(handle, name);
 
@@ -52,7 +48,7 @@ namespace URLL
 
     // UntitledImGuiFramework Event Safety - Any time
     template<typename T>
-    UVK_PUBLIC_TMPL_API void* dlsym_val(void* handle, const char* name, T* var) noexcept
+    void* dlsym_val(void* handle, const char* name, T* var) noexcept
     {
         var = (T*)dlsym(handle, name);
         return (var == nullptr ? nullptr : handle);
@@ -60,7 +56,7 @@ namespace URLL
 
     // UntitledImGuiFramework Event Safety - Any time
     template<typename T>
-    UVK_PUBLIC_TMPL_API void* dlsym_func(void* handle, const char* name, T& var) noexcept
+    void* dlsym_func(void* handle, const char* name, T& var) noexcept
     {
         *(void**)(var) = dlsym(handle, name);
         return (var == nullptr ? nullptr : handle);
@@ -68,9 +64,9 @@ namespace URLL
 
     // returns 0 on success, everything else must be an error
     // UntitledImGuiFramework Event Safety - Any time
-    UVK_PUBLIC_API int dlclose(void* handle) noexcept;
+    MLS_PUBLIC_API int dlclose(void* handle) noexcept;
 
     // returns a string with the corresponding error, if there is no error it returns null
     // UntitledImGuiFramework Event Safety - Any time
-    UVK_PUBLIC_API char* dlerror() noexcept;
+    MLS_PUBLIC_API char* dlerror() noexcept;
 }
